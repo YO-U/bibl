ce91c2da3bce4c009c8d5eba3266c09ef18f21d3
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-12-21 12:09:30 +0200

    Добавьте файлы проекта.

---
+++ bibl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyLib;
+namespace bibl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[3, 6];
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    mas[i, j] = rnd.Next(-100, 100);
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int res = MyLib.ClassLib.SumNegative(mas);
+        }
+    }
+}
---
+++ bible/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class ClassLib
+    {
+        public static int SumNegative(int[,] mas)
+        {
+
+            
+            int max = mas[0, 0];
+            int min = mas[0, 0];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(); 
+                for (int j = 0; j < 6; j++)
+                {
+                    Console.Write(mas[i, j] + " ");
+                    if (mas[i, j] > max)
+                        max = mas[i, j];
+
+                    if (mas[i, j] < min)
+                        min = mas[i, j];                                      
+                }
+            }  
+            int S = 0;
+            S += max + min;
+            Console.WriteLine();
+            Console.WriteLine("Макс {0}",max);
+            Console.WriteLine("Мин {0}",min);
+                    Console.WriteLine("Сумма {0}",S);
+            return max;
+            return min;
+
+        }
+    }
+}
